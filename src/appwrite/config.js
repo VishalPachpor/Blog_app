7df525288d779163a0070b455f1b14d6cf8b0d59
VishalPachpor <Vishalpatil080502@gmail.com>
2023-11-21 21:38:05 +0530

    Create post

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -5,7 +5,33 @@ export class Service{
     client = new Client();
     database;
     bucket;
-    
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.projectId);
+        this.database = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+    async createPost({title, slug,content, featuredImage, status, userId}){
+        try {
+            const post = await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            console.log(`Appwrite service :: create post error :: ${error}`)
+        }
+    }
+
 }
 
 const service = new Service()
